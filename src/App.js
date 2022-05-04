1c3cfd9a7c5d09aa5f739bd68c4866e2fd0fbab5
Tautvydas Tunkis <ttunkis@gmail.com>
2022-05-04 16:18:14 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Block, Button, Hero } from './components';
 function App() {
   return (
     <div className='App'>
-      <Hero title='This is hero title' subtitle='This is hero subtitle'></Hero>
+      <Hero color='danger' title='This is hero title' subtitle='This is hero subtitle'></Hero>
       <Block>First block</Block>
       <Block color='success'>Second block</Block>
       <Block color='danger'>Third block</Block>
